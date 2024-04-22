e1cd264483db484ef1b20cb8973bf4d8771d67ce
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-22 12:18:52 +0200

    adding items

--- src/scenes/AddProduct/index.jsx
+++ src/scenes/AddProduct/index.jsx
@@ -29,8 +29,37 @@ const AddProduct = () => {
   const handleFormSubmit = async (values) => {
     console.log(values);
   };
-  const Add_Product = () => {
+  const Add_Product = async() => {
     console.log(productDetails);
+    let responseData;
+    let product=productDetails;
+
+    let formData=new FormData();
+    formData.append('product',image);
+
+    await fetch('http://localhost:5000/upload',{
+      method: 'POST',
+      headers: {
+        Accept: 'application/json'
+      },
+      body: formData,
+    }).then((resp)=>resp.json()).then((data)=>{responseData=data})
+
+    if(responseData.success){
+      product.prodImage=responseData.image_url;
+      console.log(product);
+      await fetch('http://localhost:5000/addproduct',{
+        method: 'POST',
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(product),
+      }).then((res)=>res.json()).then((data)=>{
+        data.success ? alert('Product Added')
+        : alert('Failed to Add Product')
+      })
+    }
   };
   const changeHandler = (e) => {
     setProductDetails({ ...productDetails, [e.target.name]: e.target.value });

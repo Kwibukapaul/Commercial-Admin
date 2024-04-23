7446545f692da3480fce298a1bb440ff3d7478a4
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-23 20:25:19 +0200

    setups

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -3,7 +3,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
+// import { mockDataTeam } from "../../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
